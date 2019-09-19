cc7a832de461e4fd8bcea7db036f6bdb4902f180
arvindbehera <arvind.behera@yahoo.co.in>
2019-09-19 19:44:03 +0530

    Changing database

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //end ponts//
-app.get('/', (req, res) => { res.send(db.users); });
+app.get('/', (req, res) => { res.send('it is working')});
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)});
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
